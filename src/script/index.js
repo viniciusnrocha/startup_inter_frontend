9eb2c6d968da055e991398333b964674261aa89c
Crispim <gabriel.crispim01@hotmail.com>
2024-04-12 15:31:09 -0300

    Javascript to navbar finished and 'hero section' started

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,16 @@
 const openMenu = document.querySelector('#open-nav')
-
+const navbar = document.querySelector('nav')
 function closeMenu () {
     openMenu.checked = false
 }
+
+function checkScrollPosition() {
+    if(document.documentElement.scrollTop === 0){
+        navbar.style.boxShadow = "0 0 0 0 transparent"
+    } else{
+        navbar.style.boxShadow = "0 0.1em 1.5em 0 #826fff65"
+    }
+}
+
+window.addEventListener("load", checkScrollPosition)
+window.addEventListener("scroll", checkScrollPosition)
